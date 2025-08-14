a577a2ccdb7732620ef8e9f00da2359f25edc794
russellbelmont <russev.odhiambo@gmail.com>
2025-08-14 19:10:46 +0300

    Add task_4.sql: Print full description of books table without DESCRIBE

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,3 @@
--- task_4.sql
-
 USE alx_book_store;
 
 SELECT 
@@ -12,7 +10,7 @@ SELECT
 FROM 
     INFORMATION_SCHEMA.COLUMNS
 WHERE 
-    TABLE_SCHEMA = DATABASE()
-    AND TABLE_NAME = 'books'
+    TABLE_SCHEMA = 'alx_book_store'
+    AND TABLE_NAME = 'Books'
 ORDER BY 
     ORDINAL_POSITION;
